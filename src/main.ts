56fd914bbfb53d0904e7b5ae7daf6deb00ea8896
Mohamed Ramadan <91095064+MohamedRamadanSaudi@users.noreply.github.com>
2024-11-30 08:36:01 +0200

    hotfix: fix CORS configuration

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+
+  app.enableCors({
+    origin: 'http://localhost:5174',
+    methods: 'GET,HEAD,PATCH,POST,DELETE, OPTIONS, PUT',
+    credentials: true,
+  });
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
 }
