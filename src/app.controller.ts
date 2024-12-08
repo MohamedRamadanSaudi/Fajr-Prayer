ee94265dd7cef909c1dbd70bfcb433687352dc02
Mohamed Ramadan <MohamedRamadanSaudi@gmail.com>
2024-12-08 13:30:20 -0800

    feat: add logger and remove morgan

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,6 @@
-import { Controller, Get, UseInterceptors } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { MorganInterceptor } from 'nest-morgan';
 
-@UseInterceptors(MorganInterceptor('combined'))
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) { }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
@@ -8,20 +8,20 @@ import { MulterModule } from '@nestjs/platform-express';
 import { CloudinaryModule } from './cloudinary/cloudinary.module';
 import { UserModule } from './user/user.module';
 import { DaysModule } from './days/days.module';
-import { MorganInterceptor, MorganModule } from 'nest-morgan';
-import { APP_INTERCEPTOR } from '@nestjs/core';
+import { LoggerMiddleware } from './logger/logger.middleware';
 
 @Module({
   imports: [
     MulterModule.register({
       dest: './uploads',
     }),
-    MorganModule, AdminModule, AuthModule, CloudinaryModule, UserModule, DaysModule],
+    AdminModule, AuthModule, CloudinaryModule, UserModule, DaysModule],
   controllers: [AppController],
-  providers: [AppService, PrismaService, {
-    provide: APP_INTERCEPTOR,
-    useClass: MorganInterceptor("combined"),
-  },],
+  providers: [AppService, PrismaService],
   exports: [PrismaService],
 })
-export class AppModule { }
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
